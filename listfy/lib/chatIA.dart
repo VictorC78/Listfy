298189a03f71bca34d18437f3881e669e2e542ab
VictorC78 <victorcarlos0000000000@gmail.com>
2024-05-06 20:24:06 -0300

    ok

---
+++ listfy/lib/chatIA.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatIAPage extends StatelessWidget {
+  const ChatIAPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ listfy/lib/notas.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotasPage extends StatelessWidget {
+  const NotasPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
