d60ba17587a4d6fd892ed29c4cbdeb882ab9e003
VictorC78 <victorcarlos0000000000@gmail.com>
2024-05-07 20:51:17 -0300

    Revert "."
    This reverts commit efa668b0dc78425d347a92a56482aba3b71ae098.

--- listfy/lib/chatIA.dart
+++ listfy/lib/chatIA.dart
@@ -20,9 +20,6 @@ class ChatIAPage extends StatelessWidget {
                       style: TextStyle(
                           color: Color.fromARGB(255, 255, 255, 255),
                           fontSize: 25)),
-                  SizedBox(
-                    height: 10,
-                  ),
                   Text(
                       "Utilize a inteligência artificial para agilizar seu trabalho. Exemplo: Crie uma pasta com uma lista de tarefas sobre arrumar a casa...",
                       style:
@@ -30,15 +27,7 @@ class ChatIAPage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
-              height: 200,
-            ),
-            Center(
-                child: Image.asset(
-              'images/chatGPT.png',
-              width: 100,
-              height: 100,
-            ))
+            Image.asset("/assets/chatGPT.png")
           ],
         ),
       ),
