bbdc5673765ca6a9eaf685ae4abf6519256ea43e
VictorC78 <victorcarlos0000000000@gmail.com>
2024-05-07 13:33:20 -0300

    ok

--- listfy/lib/chatIA.dart
+++ listfy/lib/chatIA.dart
@@ -5,6 +5,31 @@ class ChatIAPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: 0.9 * MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("ChatGPT",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 25)),
+                  Text(
+                      "Utilize a inteligência artificial para agilizar seu trabalho. Exemplo: Crie uma pasta com uma lista de tarefas sobre arrumar a casa...",
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 136, 136, 136)))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
